1e3289040999bed09e969b68c8b4acedf67ba6b2
gersonnascimento <gerson.nascimento@locaweb.com.br>
2017-11-26 12:04:43 -0200

    foi

---
+++ JogoRemove/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace JogoRemove
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(277, 233);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Location = new System.Drawing.Point(314, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(277, 233);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel3.Controls.Add(this.button9);
+            this.panel3.Controls.Add(this.button8);
+            this.panel3.Location = new System.Drawing.Point(617, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(277, 233);
+            this.panel3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(23, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 76);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(177, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 76);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(23, 131);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 76);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(177, 131);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 76);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Red;
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(24, 20);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 76);
+            this.button5.TabIndex = 1;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Red;
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(167, 20);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 76);
+            this.button6.TabIndex = 2;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Red;
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(99, 131);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 76);
+            this.button7.TabIndex = 3;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Red;
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(25, 20);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 76);
+            this.button8.TabIndex = 1;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Red;
+            this.button9.Enabled = false;
+            this.button9.Location = new System.Drawing.Point(188, 20);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 76);
+            this.button9.TabIndex = 2;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(410, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 30);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnIniciar.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnIniciar.Location = new System.Drawing.Point(12, 281);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(174, 56);
+            this.btnIniciar.TabIndex = 4;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button10.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button10.Location = new System.Drawing.Point(688, 276);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(192, 56);
+            this.button10.TabIndex = 5;
+            this.button10.Text = "Passa a vez";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 344);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ JogoRemove/Form1.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace JogoRemove
+{
+    
+    public partial class Form1 : Form
+    {
+
+        SoundPlayer simpleSound = new SoundPlayer(@"D:\sefWav.wav");
+        int jogada = 0;
+        int cont = 0;
+
+
+        public void reiniciar()
+        {
+            this.button1.Visible = true;
+            this.button2.Visible = true;
+            this.button3.Visible = true;
+            this.button4.Visible = true;
+            this.button5.Visible = true;
+            this.button5.Visible = true;
+            this.button6.Visible = true;
+            this.button7.Visible = true;
+            this.button8.Visible = true;
+            this.button9.Visible = true;
+        }
+        public void trocaJog()
+        {
+            if (label1.Text == "Jogador 1")
+                label1.Text = "Jogador 2";
+            else if (label1.Text == "Jogador 2")
+                label1.Text = "Jogador 1";
+        }
+        public void desabilita1()
+        {
+            this.button5.Enabled = false;
+            this.button6.Enabled = false;
+            this.button7.Enabled = false;
+            this.button8.Enabled = false;
+            this.button9.Enabled = false;
+        }
+        public void desabilita2()
+        {
+            this.button1.Enabled = false;
+            this.button2.Enabled = false;
+            this.button3.Enabled = false;
+            this.button4.Enabled = false;
+            this.button8.Enabled = false;
+            this.button9.Enabled = false;
+        }
+        public void desabilita3()
+        {
+            this.button1.Enabled = false;
+            this.button2.Enabled = false;
+            this.button3.Enabled = false;
+            this.button4.Enabled = false;
+            this.button5.Enabled = false;
+            this.button6.Enabled = false;
+            this.button7.Enabled = false;
+
+        }
+
+        public void habilitabtn()
+        {
+            this.button1.Enabled = true;
+            this.button2.Enabled = true;
+            this.button3.Enabled = true;
+            this.button4.Enabled = true;
+            this.button5.Enabled = true;
+            this.button6.Enabled = true;
+            this.button7.Enabled = true;
+            this.button8.Enabled = true;
+            this.button9.Enabled = true;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Jogador 1";
+            label1.Visible = true;
+            this.habilitabtn();
+            this.habilitabtn();
+            jogada = 0;
+            cont = 0;
+            this.btnIniciar.Enabled = false;
+            reiniciar();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //simpleSound.Stop();
+            this.desabilita1();
+            this.button1.Visible = false;
+            cont++;
+            jogada++;
+            if(cont!=0)
+                this.button10.Enabled = true;
+            if (cont == 3)
+            {
+               // if (jogada == 8)
+                 //   simpleSound.Play();
+                this.trocaJog();
+                MessageBox.Show("Passou a vez!");
+                cont = 0;
+                habilitabtn();
+                this.button10.Enabled = false;
+            }
+            if (jogada == 9)
+            {
+                MessageBox.Show(label1.Text + " perdeu!!");
+                this.btnIniciar.Text = "Reiniciar";
+                this.btnIniciar.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            simpleSound.Stop();
+            this.button2.Visible = false;
+            this.desabilita1();
+            cont++;
+            jogada++;
+            if (cont != 0)
+                this.button10.Enabled = true;
+            if (cont == 3) { 
+               // if (jogada == 8)
+                    //simpleSound.Play();
+            
+                this.trocaJog();
+                MessageBox.Show("Passou a vez!");
+                cont = 0;
+                habilitabtn();
+                this.button10.Enabled = false;
+            }
+            if (jogada == 9)
+            {
+                MessageBox.Show(label1.Text + " perdeu!!");
+                this.btnIniciar.Text = "Reiniciar";
+                this.btnIniciar.Enabled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            simpleSound.Stop();
+            this.button3.Visible = false;
+            this.desabilita1();
+            cont++;
+            jogada++;
+            if (cont != 0)
+                this.button10.Enabled = true;
+            if (cont == 3)
+            {
+             //   if (jogada == 8)
+               //     simpleSound.Play();
+                this.trocaJog();
+                MessageBox.Show("Passou a vez!");
+                cont = 0;
+                habilitabtn();
+                this.button10.Enabled = false;
+            }
+            if (jogada == 9)
+            {
+                MessageBox.Show(label1.Text + " perdeu!!");
+                this.btnIniciar.Text = "Reiniciar";
+                this.btnIniciar.Enabled = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            simpleSound.Stop();
+            this.button4.Visible = false;
+            this.desabilita1();
+            cont++;
+            jogada++;
+            if (cont != 0)
+                this.button10.Enabled = true;
+            if (cont == 3)
+            {
+               // if (jogada == 8)
+                 //   simpleSound.Play();
+                this.trocaJog();
+                MessageBox.Show("Passou a vez!");
+                cont = 0;
+                habilitabtn();
+                this.button10.Enabled = false;
+            }
+            if (jogada == 9)
+            {
+                MessageBox.Show(label1.Text + " perdeu!!");
+                this.btnIniciar.Text = "Reiniciar";
+                this.btnIniciar.Enabled = true;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            simpleSound.Stop();
+            this.button5.Visible = false;
+            this.desabilita2();
+            cont++;
+            jogada++;
+            if (cont != 0)
+                this.button10.Enabled = true;
+            if (cont == 3)
+            {
+               // if (jogada == 8)
+                 //   simpleSound.Play();
+                this.trocaJog();
+                MessageBox.Show("Passou a vez!");
+                cont = 0;
+                habilitabtn();
+                this.button10.Enabled = false;
+            }
+            if (jogada == 9)
+            {
+                MessageBox.Show(label1.Text + " perdeu!!");
+                this.btnIniciar.Text = "Reiniciar";
+                this.btnIniciar.Enabled = true;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            simpleSound.Stop();
+            this.button6.Visible = false;
+            this.desabilita2();
+            cont++;
+            jogada++;
+            if (cont != 0)
+                this.button10.Enabled = true;
+            if (cont == 3)
+            {
+               // if (jogada == 8)
+                 //   simpleSound.Play();
+                this.trocaJog();
+                MessageBox.Show("Passou a vez!");
+                cont = 0;
+                habilitabtn();
+                this.button10.Enabled = false;
+            }
+            if (jogada == 9)
+            {
+                MessageBox.Show(label1.Text + " perdeu!!");
+                this.btnIniciar.Text = "Reiniciar";
+                this.btnIniciar.Enabled = true;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            simpleSound.Stop();
+            this.button7.Visible = false;
+            this.desabilita2();
+            cont++;
+            jogada++;
+            if (cont != 0)
+                this.button10.Enabled = true;
+            if (cont == 3)
+            {
+              //  if (jogada == 8)
+                //    simpleSound.Play();
+                this.trocaJog();
+                MessageBox.Show("Passou a vez!");
+                cont = 0;
+                habilitabtn();
+                this.button10.Enabled = false;
+            }
+            if (jogada == 9)
+            {
+                MessageBox.Show(label1.Text + " perdeu!!");
+                this.btnIniciar.Text = "Reiniciar";
+                this.btnIniciar.Enabled = true;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            simpleSound.Stop();
+            this.button8.Visible = false;
+            this.desabilita3();
+            cont++;
+            jogada++;
+            if (cont != 0)
+                this.button10.Enabled = true;
+            if (cont == 3)
+            {
+               // if (jogada == 8)
+                 //   simpleSound.Play();
+                this.trocaJog();
+                MessageBox.Show("Passou a vez!");
+                cont = 0;
+                habilitabtn();
+                this.button10.Enabled = false;
+            }
+            if (jogada == 9)
+            {
+                MessageBox.Show(label1.Text + " perdeu!!");
+                this.btnIniciar.Text = "Reiniciar";
+                this.btnIniciar.Enabled = true;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            simpleSound.Stop();
+            this.button9.Visible = false;
+            this.desabilita3();
+            cont++;
+            jogada++;
+            if (cont != 0)
+                this.button10.Enabled = true;
+            if (cont == 3)
+            {
+              //  if (jogada == 8)
+                //    simpleSound.Play();
+                this.trocaJog();
+                MessageBox.Show("Passou a vez!");
+                this.button10.Enabled = false;
+                cont = 0;
+                habilitabtn();
+                this.button10.Enabled = false;
+            }
+            if (jogada == 9)
+            {
+                MessageBox.Show(label1.Text + " perdeu!!");
+                this.btnIniciar.Text = "Reiniciar";
+                this.btnIniciar.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //if (jogada == 8)
+              //  simpleSound.Play();
+            this.trocaJog();
+            cont = 0;
+            habilitabtn();
+            this.button10.Enabled = false;
+            
+        }
+    }
+}
